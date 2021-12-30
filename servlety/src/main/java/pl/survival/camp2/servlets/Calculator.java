fdf21315986da9d13ed437d3d70194e034d7a226
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2021-12-30 15:44:50 +0100

    new projects

---
+++ servlety/src/main/java/pl/survival/camp2/servlets/Calculator.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package pl.survival.camp2.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jerzy.drobot
+ */
+@WebServlet(name = "Calculator", urlPatterns = {"/calc"})
+public class Calculator extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Calculator</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Calculator at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter writer = response.getWriter();
+        String arga = request.getParameter("a");
+        String argb = request.getParameter("b");
+        try {    
+        int a = Integer.parseInt(arga);
+        int b = Integer.parseInt(argb);
+        writer.println("Wynik = " + (a + b));
+        writer.println("<br/><a href='index.html'>Powrót</a>");
+        } catch (NumberFormatException nfe) {
+            writer.println("Błędne dane");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+
+// </editor-fold>
+
+}
